edded56252198c130de5e61156513d93d3b4449a
Pat Hermens <p@hermens.com.au>
2019-12-05 12:13:28 +1100

    Initial spotify implementation

---
+++ src/Streetlight.Base/Spotify.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using SpotifyAPI.Web.Models;
+
+namespace Streetlight.Base
+{
+    internal class Spotify
+    {
+        private const string _clientId = "ae8e2b78a5c145d6af9c9e26a1f91c0c";
+        private static SpotifyWebAPI _spotify;
+        private bool IsAuthenticated = false;
+
+        public Spotify()
+        {
+            var auth = new ImplicitGrantAuth(
+                _clientId,
+                "http://localhost:5000",
+                "http://localhost:5000"
+              );
+            auth.AuthReceived += (sender, payload) =>
+            {
+                Console.WriteLine("## Auth token received");
+                auth.Stop(); // `sender` is also the auth instance
+                _spotify = new SpotifyWebAPI()
+                {
+                    TokenType = payload.TokenType,
+                    AccessToken = payload.AccessToken
+                };
+                IsAuthenticated = true;
+            };
+            Console.WriteLine("## Requesting auth token");
+            auth.Start(); // Starts an internal HTTP Server
+            auth.OpenBrowser();
+            while (!IsAuthenticated) { }; // Wait for the auth-token to come back.
+        }
+
+        public void GetTrack()
+        {
+            FullTrack track = _spotify.GetTrack("3Hvu1pq89D4R0lyPBoujSv");
+            Console.WriteLine($"Track: '{track.Name}' by '{track.Artists.First().Name}'");
+        }
+
+        public void GetRandomTrack()
+        {
+            var query = SearchTerm.GetNext(false);
+            var lookingFor = SpotifyAPI.Web.Enums.SearchType.All;
+            Console.WriteLine($"Searching for '{query}'... ");
+
+            var r = _spotify.SearchItems(query, lookingFor, limit: 1);
+            if (r.HasError())
+            {
+                Console.WriteLine($"Spotify failed: {r.Error.Status} - {r.Error.Message}");
+            }
+            else if (r?.Tracks?.Items.Count > 0)
+            {
+                Console.WriteLine($"Spotify returned {r?.Tracks?.Items.Count} tracks.");
+                var track = r.Tracks?.Items?.FirstOrDefault();
+                if (track != null) Console.WriteLine($"First track: '{track.Name}' by '{track.Artists.First().Name}'");
+            }
+        }
+    }
+}
